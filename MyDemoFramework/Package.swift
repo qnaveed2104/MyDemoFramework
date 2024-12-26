7c8fc706c2e40124f131c876c415a7123d538f2b
qazi <qazi.naveed@nextgeni.com>
2024-12-26 17:23:52 +0100

    add swift package

---
+++ MyDemoFramework/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "MyDemoFramework",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "MyDemoFramework",
+            targets: ["MyDemoFramework"]),
+    ],
+    targets: [
+        .target(
+            name: "MyDemoFramework",
+            path: "Source",
+            exclude: ["Tests"]),
+        .testTarget(
+            name: "MyDemoFrameworkTests",
+            dependencies: ["MyDemoFramework"],
+            path: "Tests"),
+    ]
+)
