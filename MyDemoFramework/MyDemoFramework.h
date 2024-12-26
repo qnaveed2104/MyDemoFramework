4313e9e254d93d486c071aa7610553aa22027676
qazi <qazi.naveed@nextgeni.com>
2024-12-26 16:55:18 +0100

    Initial Commit

---
+++ MyDemoFramework/MyDemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyDemoFramework.h
+//  MyDemoFramework
+//
+//  Created by Qazi on 26/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDemoFramework.
+FOUNDATION_EXPORT double MyDemoFrameworkVersionNumber;
+
+//! Project version string for MyDemoFramework.
+FOUNDATION_EXPORT const unsigned char MyDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDemoFramework/PublicHeader.h>
+
+
