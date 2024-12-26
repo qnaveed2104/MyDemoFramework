385909645e783d39c5fbbd7289b5cf12531110ac
qazi <qazi.naveed@nextgeni.com>
2024-12-26 17:30:33 +0100

    initial commit

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "MyDemoFramework",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyDemoFramework",
             targets: ["MyDemoFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyDemoFramework"),
+            name: "MyDemoFramework",
+            path: "Source",
+            exclude: ["Tests"]),
         .testTarget(
             name: "MyDemoFrameworkTests",
-            dependencies: ["MyDemoFramework"]
-        ),
+            dependencies: ["MyDemoFramework"],
+            path: "Tests"),
     ]
 )
